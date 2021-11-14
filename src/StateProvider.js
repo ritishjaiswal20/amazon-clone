690bb9a5f7888d67dff68add1e31d5056256f125
Ritish jaiswal <ritishjaiswal20@gmail.com>
2021-11-14 13:17:45 +0530

    redux added

---
+++ src/StateProvider.js
@@ -0,0 +1,10 @@
+import React, { createContext, useContext, useReducer } from 'react'
+export const StateContext = createContext()
+// wrap our app and provide the data layer
+export const StateProvider = ({ reducer, intialState, children }) => (
+  <StateContext.Provider value={useReducer(reducer, intialState)}>
+    {children}
+  </StateContext.Provider>
+)
+
+export const useStateValue = () => useContext(StateContext)
